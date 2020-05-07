2f9bc93da09f9ed4a7635c2bd4f236e2528aa33b
Jamsine <jasmine.r.davies@gmail.com>
2020-05-07 15:58:22 +0100

    Added #touch_in and #touch_out, MIN_BALANCE constant and made #deduct_money private. Created relevant tests to guide their development.

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,15 +1,31 @@
 class Oystercard
   MAX_BALANCE = 90
+  MIN_BALANCE = 1
+
   attr_accessor :balance
+  attr_accessor :in_journey
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
     return raise "Error: Cannot top-up balance beyond #{MAX_BALANCE} current balance is #{self.balance}" if self.balance + amount > MAX_BALANCE
     self.balance += amount
   end
+
+  def touch_in(station)
+    return raise "Insufficient funds. Minimum balance is #{MIN_BALANCE}" if self.balance < MIN_BALANCE
+    self.in_journey = true
+  end
+
+  def touch_out
+    self.in_journey = false
+    self.deduct_money(MIN_BALANCE)
+  end
+
+  private
 
   def deduct_money(amount)
     self.balance -= amount
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -36,4 +36,61 @@ RSpec.describe Oystercard do
 
     expect(card.balance).to eq(48)
   end
+
+  it 'in_journey? is initialized at false' do
+    card = Oystercard.new
+
+    expect(card.in_journey).to eq(false)
+  end
+
+  it 'can touch in card' do
+    card = Oystercard.new
+    station = double(:Station)
+
+    card.top_up(50)
+    card.touch_in(station)
+
+    expect(card.in_journey).to be(true)
+  end
+
+  it "can touch out card" do
+    card = Oystercard.new
+    station = double(:Station)
+
+    card.top_up(50)
+    card.touch_in(station)
+    card.touch_out
+
+    expect(card.in_journey).to be(false)
+  end
+
+  it 'knows the minimum balance' do
+    expect(Oystercard::MIN_BALANCE).to eq(1)
+  end
+
+  it 'refuses touch in if balance is less than minimum balance' do
+    card = Oystercard.new
+
+    expect { card.touch_in }.to raise_error("Insufficient funds. Minimum balance is #{Oystercard::MIN_BALANCE}")
+  end
+
+  it 'deducts money from balance when the journey has ended.' do
+    card = Oystercard.new
+    station = double(:Station)
+    initial_balance = card.top_up(50)
+    card.touch_in(station)
+
+    expect { card.touch_out }.to change { card.balance }.by((Oystercard::MIN_BALANCE * -1))
+  end
+
+  it 'records entry station upon touching in.' do
+    card = Oystercard.new
+    station = double(:Station)
+
+    card.top_up(50)
+    card.touch_in(station)
+
+    expect(card.entry_station).to eq(station)
+
+  end
 end
